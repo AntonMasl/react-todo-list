70368ad09740610f42a4e32531de034f57918730
Maslov Anton <tosha.maslov.1999@mail.ru>
2020-09-12 12:51:03 +0300

    Updates

---
+++ precache-manifest.f863b27fc50dab886bae1e29118bd41f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3342875d42e97c9a3cc0ba0fd2822edd",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "70bb30229719fbdb7cbd",
+    "url": "/react-todo-list/static/css/main.79a578b0.chunk.css"
+  },
+  {
+    "revision": "715c981b7cd57b4d0c6e",
+    "url": "/react-todo-list/static/js/2.4124bcf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.4124bcf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70bb30229719fbdb7cbd",
+    "url": "/react-todo-list/static/js/main.e62d5846.chunk.js"
+  },
+  {
+    "revision": "19572fa80932addc344b",
+    "url": "/react-todo-list/static/js/runtime-main.3f840a61.js"
+  }
+]);
